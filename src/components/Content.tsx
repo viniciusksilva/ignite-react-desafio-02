f101398e9bdf2ae18b52f93f6d07e1841e96078f
Vinicius <viniciusksilva@gmail.com>
2021-03-26 12:23:30 -0300

    Resolucao do desafio complementar 01

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,47 @@
-export function Content() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+
+import { MovieCard } from './MovieCard';
+
+import '../styles/content.scss';
+
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface ContentProps {
+  contentGenre: GenreResponseProps;
+  contentMovies: MovieProps[];
+}
+
+export function Content({contentGenre, contentMovies}: ContentProps) {
+
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {contentGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {contentMovies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+    </div>
+  )
 }
